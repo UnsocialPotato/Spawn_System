e0a0076fef9b815a6b9828f77ae911bcb78b1ae4
Katelyn Ripa <77808182+UnsocialPotato@users.noreply.github.com>
2022-10-30 21:06:31 -0700

    Add files via upload

---
+++ Scripts/IVehicle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVehicle { }
+
+public class Unicycle : IVehicle { }
+public class Bicycle : IVehicle { }
+public class Tandem : IVehicle { }
+public class Tricycle : IVehicle { }
+public class GoKart : IVehicle { }
+public class FamilyBike : IVehicle { }
+public class Car : IVehicle { }
+public class Motorcycle : IVehicle { }
+public class Truck : IVehicle { }
